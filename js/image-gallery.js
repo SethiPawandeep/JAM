4c5525bd205322a475f9c528559a840b769c72fe
pawandeep2007 <pawandeep2007@gmail.com>
2016-11-17 10:37:01 +0530

    Complete for WSD Internal. To continue as a bigger project

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -1,29 +1,28 @@
-window.onload = function () {
-
-
-//    (function () {
-        var Variables = {
-                numOfIssues: 8
+(function () {
+    var Variables = {
+            numOfIssues: 8
+        },
+        DOM = {
+            'img-gallery': document.querySelector('.img-gallery'),
+            'img-over': document.querySelector('.img-over')
+        },
+        Functions = {
+            openImage: function (thisImage) {
+                //                var divFixed = '';
+                DOM['img-over'].style.display = 'block';
+                DOM['img-over'].innerHTML.src = thisImage.src;
             },
-            DOM = {
-                'img-gallery': document.querySelector('.img-gallery'),
-                'img-over': document.querySelector('.img-over')
-            },
-            Functions = {
-                openImage: function (thisImage) {
-                    //                var divFixed = '';
-                    DOM['img-over'].style.display = 'block';
-                    DOM['img-over'].innerHTML.src = thisImage.src;
-                },
-                loadGallery: function () {
-                    var images = '';
-                    var i;
-                    for (i = 1; i <= Variables.numOfIssues; i++) {
-                        images = images + '<div class="col-lg-3 pull-left"><div class="col-lg-2"></div><img onclick="this.openImage(this);" class="issue-cover" src="img/Issues/' + i + '.jpg"></div>'
-                    }
-                    DOM['img-gallery'].innerHTML = images;
+            loadGallery: function () {
+                var images = '';
+                var i;
+                for (i = 1; i <= Variables.numOfIssues; i++) {
+                    images = images + '<div class="col-lg-3 pull-left"><div class="col-lg-2"></div><img onclick="console.log("clicked");" class="issue-cover" src="img/Issues/' + i + '.jpg"></div>';
                 }
-            };
-Functions.loadGallery();
-//    })();
-}
+                DOM['img-gallery'].innerHTML = images;
+            }
+        };
+
+    window.onload = function () {
+        Functions.loadGallery();
+    }
+})();
