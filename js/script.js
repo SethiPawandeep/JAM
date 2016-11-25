1aa7b9bd98d5f5a5eadb911235087d946087fafe
pawandeep2007 <pawandeep2007@gmail.com>
2016-11-25 23:26:15 +0530

    Scroll bar styling.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+function createForm() {
+    var x = document.querySelector('.buy-now-form').innerHTML;
+    x = '<span style="font-size: 2em; position: absolute; right: 20px; top:15px;" class="cross" onmouseover="cursor: pointer" onclick="closeBuyNowForm();">&times;</span><h2 style="margin-left: 20px;font-family: sans-serif; font-weight: 300">Buy now | JAM 12</h2><hr/><form><label for="fname">First Name</label><input type="text" id="fname" placeholder="First Name"><label for="lname">Last Name</label><input type="text" id="lname" placeholder="Last Name"><label for="hostelNumber">Hostel Number: </label><input type="text" id="hostel-number" placeholder="Hostel Number, Bhawan"><label for="phoneNumber">Phone Number: </label><input type="text" id="phone-number" placeholder="Phone Number"></form>' + x;
+    document.querySelector('.buy-now-form').innerHTML = x;
+    document.querySelector('.cross').style.cursor = 'pointer';
+}
+
 function buyNow() {
     /*
         to set CSS for buy-now-box
@@ -9,22 +16,18 @@ function buyNow() {
     var buyNowForm = document.querySelector('.buy-now-form').style;
     buyNowForm.position = 'fixed';
     buyNowForm.zIndex = '3';
-    buyNowForm.height = '500px';
+    buyNowForm.height = '70vh';
     buyNowForm.width = '80vw';
     buyNowForm.left = '10vw';
+    buyNowForm.overflowY = 'scroll';
+    buyNowForm.overflowX = 'hidden';
     buyNowForm.top = '80px';
-    buyNowForm.borderRadius = '40px';
-    buyNowForm.border = '1px solid blue';
+    buyNowForm.borderRadius = '10px';
+    buyNowForm.border = '2px solid lightblue';
     buyNowForm.boxShadow = '3px 3px 3px lightblue';
+    buyNowForm.transition = "ease .5s";
     buyNowForm.background = 'rgba(255,255,255,0.95)';
     createForm();
-}
-
-function createForm() {
-    var x = document.querySelector('.buy-now-form').innerHTML;
-    x = '<span style="font-size: 2em; position: absolute; right: 20px;" class="cross" onmouseover="cursor: pointer" onclick="closeBuyNowForm();">&times;</span><h2 style="margin-left: 20px;font-family: sans-serif; font-weight: 300">Buy now | JAM 12</h2><form><label for="fname">First Name</label><input type="text" id="fname" placeholder="First Name"><label for="lname">Last Name</label><input type="text" id="lname" placeholder="Last Name"></form>' + x;
-    document.querySelector('.buy-now-form').innerHTML = x;
-    document.querySelector('.cross').style.cursor = 'pointer';
 }
 
 function closeBuyNowForm() {
